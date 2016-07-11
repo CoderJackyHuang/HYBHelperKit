9e5c8794807f59953aaf3892ecf298efae0e98f7
huangyibiao <huangyibiao@163.com>
2016-07-11 11:27:24 +0800

    update

--- HYBHelperKitSources/HYBHelperKit.h
+++ HYBHelperKitSources/HYBHelperKit.h
@@ -9,11 +9,12 @@
 /**
  *	使用此库的入口，可以通过引入此文件，达到统一引入全部文件的上的
  */
-#import "HYBCommonKit.h"
+
 #import "HYBHelperFoundationKit.h"
 #import "HYBHelperBlocksKit.h"
 #import "HYBHelperMasonryMaker.h"
 #import "HYBHelperUIKit.h"
+#import "HYBCommonKit.h"
 
 #import "HYBHelperKitBaseController.h"
 #import "HYBHelperKitBaseURLController.h"
